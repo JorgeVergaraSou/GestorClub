d47a5b8a53f965a60f3c165c199797bac3d18f53
JorgeVergaraSou <110506751+JorgeVergaraSou@users.noreply.github.com>
2023-03-29 22:45:11 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import GestorClub from "./GestorClub";
 
 let chile = new GestorClub()
-
-//chile.agregarSocio()
-
+/*
+chile.agregarSocio();
+chile.agregarSocio();
+chile.agregarSocio();
+chile.agregarSocio();*/
 //console.log(chile.data);
 
 chile.buscarXNombre('ARTURO');
-
+chile.buscarXApellido('sanchez');
+chile.buscarXDocumento('22222');
+chile.buscarXTelefono('2494123789');
